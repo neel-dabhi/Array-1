70b1cf44e69dbb0fe675c7ebbdc562796e981bc9
neel-dabhi <yoursdabhineelkanth@gmail.com>
2025-05-27 16:13:38 -0300

    Completed Array 1

---
+++ ProductOfArray.java
@@ -0,0 +1,20 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// Runs on LeetCode: YES
+class ProductOfArray {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int runningProduct = 1;
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            runningProduct = runningProduct * nums[i - 1];
+            result[i] = runningProduct;
+        }
+        runningProduct = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            runningProduct = runningProduct * nums[i + 1];
+            result[i] = result[i] * runningProduct;
+        }
+        return result;
+    }
+}
